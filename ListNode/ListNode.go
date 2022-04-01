aa5e8e1bcafd4c70d1151393de713e38e5519b99
IanChenV <chenkeng45@gmail.com>
2022-04-01 17:17:11 +0800

    up

---
+++ ListNode/ListNode.go
@@ -0,0 +1,4 @@
+package structure
+
+type ListNode struct {
+}
---
+++ SegmentTree/SegemtTree.go
@@ -0,0 +1,22 @@
+package structure
+
+type TreeNode struct {
+	Val      int
+	LeftVal  int
+	RightVal int
+	Left     *TreeNode
+	Right    *TreeNode
+}
+
+// 后序遍历
+func buildTree(nums []int, left, right int) *TreeNode {
+	if left == right {
+		return &TreeNode{
+			Val:      nums[left],
+			LeftVal:  left,
+			RightVal: right,
+		}
+	}
+
+	return &TreeNode{}
+}
